cec7e1def9d16acfaa8dfd2e9dec6c6d136b6451
Gaurav <gmi3024@gmail.com>
2016-05-20 12:19:14 +0530

    clean up

--- main.rb
+++ main.rb
@@ -6,10 +6,12 @@ board        = Board.new
 player       = UserPlayer.instance(board)
 robot_player = RobotPlayer.instance(board)
 
-# player.move(0, 0)
-p board.player_at(1,1)
+player.move(0, 0)
+player.move(1, 1)
+player.move(2, 2)
+
+p board.who_won?
 
 # robot_player.move
-# player.move(2, 2)
-
+# board.player_at(1,1)
 # print board
--- src/board.rb
+++ src/board.rb
@@ -13,10 +13,6 @@ class Board
                 ['r2c0', 'r2c1', 'r2c2']  ]
   end
 
-  def empty_position(row)
-    row.find{|e| e.match(/r\dc\d/) }
-  end
-
   def position_empty?(position)
     !position.match(/r\dc\d/).nil?
   end
@@ -41,30 +37,34 @@ class Board
     winning_row = @matrix.transpose.find{ |row| row.select{|p| p.to_s == player.to_s }.length == 2 }
     index(player_not_present_at_element(winning_row)) if winning_row
   end
-    
-  def left_diagonal(player)
-    diagonal = (0..@matrix.length-1).collect{|i| @matrix[i][i] }
-    player_winning_count = diagonal.select{|p| p.to_s == player.to_s }.count
 
-    if player_winning_count == 2
-        return index(player_not_present_at_element(diagonal))
-    end
+  def left_diagonal
+    (0..@matrix.length-1).collect{|i| @matrix[i][i] }
   end
 
-  def right_diagonal(player)
+  def right_diagonal
     offset = @matrix.length
-    diagonal = (0..@matrix.length-1).collect{|i| offset=offset-1; @matrix[i][offset] }
+    (0..@matrix.length-1).collect{|i| offset=offset-1; @matrix[i][offset] }
+  end
+
+  def empty_position_on_left_diagonal(player)
+    diagonal = left_diagonal
     player_winning_count = diagonal.select{|p| p.to_s == player.to_s }.count
-    if player_winning_count == 2
-        return index(player_not_present_at_element(diagonal))
-    end
+
+    index(player_not_present_at_element(diagonal)) if player_winning_count == 2
+  end
+
+  def empty_position_on_right_diagonal(player)
+    diagonal = right_diagonal
+    player_winning_count = diagonal.select{|p| p.to_s == player.to_s }.count
+    
+    index(player_not_present_at_element(diagonal)) if player_winning_count == 2
   end
 
   def check_diagonals(player)
-    position = left_diagonal(player)
+    position = empty_position_on_left_diagonal(player)
     return position if position
-
-    right_diagonal(player)
+    empty_position_on_left_diagonal(player)
   end
     
   def is_winning?(player)
@@ -80,7 +80,7 @@ class Board
   end
 
   def each_row(&block)
-        @matrix.each(&block)
+    @matrix.each(&block)
   end
 
   def each_column(&block)
@@ -88,12 +88,14 @@ class Board
   end
 
   def each_digonal(&block) 
+    [left_diagonal, right_diagonal].each(&block)
   end
 
-  def whoAon?
-  end
-
-  def takeBack
+  def who_won?
+    return winner_on_the_row[0] if winner_on_the_row
+    return winner_on_the_column[0] if winner_on_the_column
+    return winner_on_the_diagonal[0] if winner_on_the_diagonal
+    return "Draw" if isDraw?
   end
 
   def isDraw?
@@ -104,10 +106,31 @@ class Board
   end
 
   private
+
+  def same_values_in_a_seq(sequence)
+    sequence if (sequence.uniq.count == 1)
+  end
+
+  def winner_on_the_row
+    winning_row = nil
+    each_row.any? { |row| winning_row = same_values_in_a_seq(row) }
+    winning_row
+  end
+
+  def winner_on_the_column
+    winning_column = nil
+    each_column.any? { |column| winning_column = same_values_in_a_seq(column) }
+    winning_column
+  end
+
+  def winner_on_the_diagonal
+    winning_d = nil
+    each_digonal.any? { |d| winning_d = same_values_in_a_seq(d) }
+    winning_d
+  end
+
   def is_board_empty?
-    @matrix.all? do |row|
-      row.all?{|position|  !position.match(/r\dc\d/).nil? }
-    end
+    @matrix.all? { |row| row.all?{|position|  !position.match(/r\dc\d/).nil? } }
   end
 end
 
